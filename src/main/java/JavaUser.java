99310ad2ae8cf46bd496daf8b141d8e8c09ab48f
dwhitney <dustin.whitney@gmail.com>
2010-05-23 16:13:46 -0400

    changed to match the class defined n ClassAndTraiExample

--- src/main/java/JavaUser.java
+++ src/main/java/JavaUser.java
@@ -1,37 +1,37 @@
 public class JavaUser{
     
-    private String username;
-    private String password;
-    private String email;
+    private String screenName;
+    private String name;
+    private String location;
     
-    public JavaUser(String username, String password, String email){
-        this.username = username;
-        this.password = password;
-        this.email = email;
+    public JavaUser(String screenName, String name, String location){
+        this.screenName = screenName;
+        this.name = name;
+        this.location = location;
     }
     
-    public String getUsername(){
-        return username;
+    public String getScreenName(){
+        return screenName;
     }
     
-    public void setUsername(String username){
-        this.username = username;
+    public void setScreenName(String screenName){
+        this.screenName = screenName;
     }
     
-    public String getPassword(){
-        return password;
+    public String getName(){
+        return name;
     }
     
-    public void setPassword(String password){
-        this.password = password;
+    public void setName(String name){
+        this.name = name;
     }
     
-    public String getEmail(){
-        return email;
+    public String getLocation(){
+        return location;
     }
     
-    public void setEmail(String email){
-        this.email = email;
+    public void setLocation(String location){
+        this.location = location;
     }
     
 }
