5080eb5fccb0e50121856490156950861c55dedf
Steven Lehrburger <memento85s@gmail.com>
2010-05-24 14:44:07 -0400

    empty file for stuff in 5e-g

---
+++ src/main/scala/ListOperations.scala
@@ -0,0 +1,24 @@
+//probably type most of this stuff into the REPL - just here for examples
+object ListOperations{
+
+    def main(args: Array[String]){
+
+        println(List("Hello","World","!"))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+    }
+}
